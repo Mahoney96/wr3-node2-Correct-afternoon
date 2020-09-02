bffe6ae58135cd10309b5f6c9c9f0df947dcc4d6
Mahoney96 <cmahoney966@gmail.com>
2020-09-01 23:46:08 -0700

    intial setup, with basic local server. No nodemon errors

---
+++ server/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const SERVER_PORT = 3993;
+
+app.listen(SERVER_PORT, () => console.log(`Ports Runnin on${SERVER_PORT}, Aright Aright Aright`));
+
