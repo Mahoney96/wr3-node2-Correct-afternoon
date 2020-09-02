668ce4bc9526aee06a2c39a2015c7b36c7ec7e25
Mahoney96 <cmahoney966@gmail.com>
2020-09-02 07:53:30 -0700

    Need to go back over Steps 5-7.

--- server/index.js
+++ server/index.js
@@ -4,8 +4,15 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json());
 
+app.use(express.json());
+app.use(express.static(_dirname + '/../publicbuild'));
+
+const messageBaseURL = "api/messages";
+app.post(messageBaseURL, mc.create);
+app.put(messageBaseURL, mc.read);
+app.get(`${messageBaseURL}/:id`, mc.update);
+app.delete(`${messageBaseURL}/:id`, mc.delete);
 const SERVER_PORT = 3993;
 
 app.listen(SERVER_PORT, () => console.log(`Ports Runnin on ${SERVER_PORT}, Aright Aright Aright`));
